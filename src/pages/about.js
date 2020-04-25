3a5c8153671cf45dce37baaf7fe4509a3bd1e29b
Dalton <20dalton00@gmail.com>
2020-04-25 13:59:11 -0400

    update about text

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-import { Heading } from 'theme-ui'
+import { Heading, Text } from 'theme-ui'
 
 export default () => (
   <Layout>
     <Heading as="h1">About Us</Heading>
+    <Text>This website was created and designed by a group of students studying mental and behavioral disorders at the University of South Carolina. This website brings resources for mental health support and training to one central location. It also provides crisis materials for those in the middle of a mental health emergency. We hope you find this helpful!</Text>
   </Layout>
 )
