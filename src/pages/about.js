ec8338b8bfa73e7ba8584277f9fd3544081f3d31
Dalton <20dalton00@gmail.com>
2020-04-30 23:03:32 -0400

    Add email to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { Link } from '../components/Link'
 
 import { Heading, Text } from 'theme-ui'
 
@@ -17,6 +18,10 @@ export default () => (
       website brings resources for mental health support and training to one
       central location. It also provides crisis materials for those in the
       middle of a mental health emergency. We hope you find this helpful!
+    </Text>
+    <Text>
+      Have a suggestion or a resource we missed? Feel free to{' '}
+      <Link to="mailto:djcraven@email.sc.edu">email the webmaster</Link>.
     </Text>
   </Layout>
 )
